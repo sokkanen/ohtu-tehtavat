d9cf1690676a879aa47af3bd4b9da4d558fe2fb1
sokkanen <joel.sokkanen@helsinki.fi>
2019-12-02 21:57:52 +0200

    Viikko 6 / Tehtävä 3

---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/Or.java
@@ -0,0 +1,19 @@
+package statistics.matcher;
+
+import statistics.Player;
+
+import java.util.Arrays;
+
+public class Or implements Matcher {
+
+    private Matcher[] matchers;
+
+    public Or(Matcher... matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    public boolean matches(Player p) {
+        return Arrays.stream(matchers).filter(m -> m.matches(p)).count() > 0;
+    }
+}
